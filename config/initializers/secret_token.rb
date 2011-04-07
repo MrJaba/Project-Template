2b916d778e1f9ab2ddcbad60c74aa78d7987ab5c
Tom Crinson <the.jaba@gmail.com>
2011-04-07 18:54:22 +0100

    Basic project setup - 2 column layout compass working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectTemplate::Application.config.secret_token = 'ce5c92cc48cb1de93f56521276c7e49f7a87472b4becab7194fc7d25ae82ced28157923a73eb7a89065336b4cee24118c20e102754ee5d30221a9f2586776ff3'
